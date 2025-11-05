9748fdc3d6ef72fd792f3b101f27086555ce874d
rafishaidul@gmail.com <ShaidulIslamRafi@users.noreply.github.com>
2025-11-05 18:53:41 +0600

    new

---
+++ Mid/labtask1.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<head>
+
+<title> Aiub </title>
+</head>
+<body>
+    <center>
+    <h1>Registration </h1> <br>
+    <h2>Final Registration </h2>
+</center>
+   Enter Your  Name:
+   <input type="text"> <br><br>
+   
+   Enter Your ID:
+   <input type="text">  <br><br>
+
+    Enter Your Course Name:
+   <input type="text"> <br><br>
+
+   Enter Your Course Section:
+   <input type="text">  <br><br>
+
+   Scholership Status:
+   <input type="radio">Scholership 
+   <input type="radio">Non Scholership <br><br>
+   UserName:
+   <input type="text"> <br><br>
+  
+   Password:
+   <input type="text"> <br><br>
+   
+   <input type="checkbox">Terms and condition
+
+<center>
+    <input type="submit">
+</center>
+
+
+
+
+    
+
+</body>
---
+++ Mid/labtask_2.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<head>
+
+<title>WT_Task 1 </title>
+
+
+</head>
+<body>
+    <center>
+  <h1 style ="color:blue;font-size:50px"> Clinic Patient Registration </h1>
+</center>
+<p style ="font-size:20px">
+Full Name:<br>
+<input type ="text"style ="font-size:20px"><br>
+Age:<br>
+<input type ="text"style ="font-size:20px"><br>
+Phone Number:<br>
+<input type ="number"style ="font-size:20px"><br>
+Email:<br>
+<input type ="text"style ="font-size:20px"><br>
+Insurance Provider:<br>
+<input type ="dropdown"style ="font-size:20px"><br>
+Insurance Policy Number:<br>
+<input type ="text"style ="font-size:20px"><br>
+</p>
+<center>
+<h2 style ="color:blue;font-size:40px">Additional Information</h2><br>
+</center>
+<p style ="font-size:20px">
+UserName:<br>
+<input type ="text"style ="font-size:20px"><br>
+Password:<br>
+<input type ="text"style ="font-size:20px"><br>
+Confirm Password:<br>
+<input type ="text"style ="font-size:20px"><br>
+</p>
+<center>
+    <input type="submit"value="Register" style ="background-color:blue;font-size:30px">
+</center>
+</body>

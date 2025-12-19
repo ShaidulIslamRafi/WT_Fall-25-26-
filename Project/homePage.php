676c916cb9b4badd5b568d3147c58a2c3c0303b8
rafishaidul@gmail.com <ShaidulIslamRafi@users.noreply.github.com>
2025-12-19 17:23:38 +0600

    a

--- Project/homePage.php
+++ Project/homePage.php
@@ -2,12 +2,51 @@
 <html>
 
 <head>
- <title> </title>
+  <meta charset="UTF-8">
+  <meta name="description" content="Hospital management">
+  <meta name="author" content="Rafi">
+  <meta name="keyword" content="Hospital,manangement">
+  <meta name="viewpoint" content ="width=device-width,initial-scal=1.0">
+
+ <title>Home </title>
+<style>
+
+  form{
+     
+
+     background-color: rgba(207, 219, 207, 0.64);
+     width:500px;
+     height:450px;
+     /*margin: 0 auto;*/
+     position:absolute;
+     top:18%;
+     right:40%;
+    
+
+
+  }
+input{
+
+     padding:8px;
+     width:80%;
+     margin-top:20px;
+
+}
+
+
+
+</style>
 
 </head>
 
 <body>
-    
+  <form action="">
+
+   <label for="">Username</label><br>
+   <input type="text" >
+
+
+  </form>  
 
 
 

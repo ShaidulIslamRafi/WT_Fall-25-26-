7a79618bc3dc59c217d81fd739af8696e9345bd8
rafishaidul@gmail.com <ShaidulIslamRafi@users.noreply.github.com>
2025-12-19 18:54:32 +0600

    a

--- Project/homePage.php
+++ Project/homePage.php
@@ -17,33 +17,51 @@
      background-color: rgba(207, 219, 207, 0.64);
      width:500px;
      height:450px;
-     /*margin: 0 auto;*/
+     margin: 0 auto;
      position:absolute;
-     top:18%;
-     right:40%;
+     top:0%;
+     right:0%;
+     bottom:0%;
+     left:0%;
+     margin:auto;
     
-
+   
 
   }
 input{
 
      padding:8px;
-     width:80%;
-     margin-top:20px;
+     width:50%;
+     /*margin:50px;*/
+     position:absolute;
+     left: 70px;
 
 }
-
-
+label{
+    
+    position:absolute;
+    left:70px;
+}
+h1{
+    text-align:center;
+}
+p{
+    text-align:center;
+}
 
 </style>
 
 </head>
 
 <body>
+    
   <form action="">
+    <h1>Login Page</h1>
+    <p>Login with your details to continue </p>
 
-   <label for="">Username</label><br>
-   <input type="text" >
+   
+   <label for="">Email:</label><br>
+   <input type="text" value="Email" >
 
 
   </form>  

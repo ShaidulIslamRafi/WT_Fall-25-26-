6eb5711485706c40f6f406146df57b89695bf544
rafishaidul@gmail.com <ShaidulIslamRafi@users.noreply.github.com>
2025-12-19 19:28:07 +0600

    up

--- Project/homePage.php
+++ Project/homePage.php
@@ -14,7 +14,7 @@
   form{
      
 
-     background-color: rgba(207, 219, 207, 0.64);
+     background-color: rgba(255, 255, 255, 1);
      width:500px;
      height:450px;
      margin: 0 auto;
@@ -34,19 +34,36 @@ input{
      width:50%;
      /*margin:50px;*/
      position:absolute;
-     left: 70px;
+     left: 100px;
+     border: 1px solid rgba(244, 244, 250, 1);
+     border-radius:10px;
 
 }
 label{
     
     position:absolute;
-    left:70px;
+    left:105px;
+}
+button{
+    background-color: #2b5ec5ff;
+    color:white;
+    width:55%;
+    height:35px;
+    position:absolute;
+    left: 22%;
+    top:60%;
+    border-radius: 10px;
+
+
 }
 h1{
     text-align:center;
 }
 p{
     text-align:center;
+}
+body{
+    background-color: #e6e6e6ff;
 }
 
 </style>
@@ -61,8 +78,12 @@ p{
 
    
    <label for="">Email:</label><br>
-   <input type="text" value="Email" >
-
+   <input type="text" placeholder="Email" ><br><br><br>
+   <label for="">Password:</label><br>
+   <input type="text " placeholder="Password"><br><br>
+   <button >Login</button>
+   <label for=""style="position:absolute;top:80%;">Don`t have an account</label>
+  
 
   </form>  
 

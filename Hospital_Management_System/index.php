878aeb7eb520c65c27bd548a3148e51408b226bc
Tousif <tousif.tasin2000@gmail.com>
2026-01-09 19:37:34 +0600

    check

---
+++ Hospital_Management_System/index.php
@@ -0,0 +1,22 @@
+<?php
+echo "I am lsnkdnn tousif <br>";
+echo"gygde<br>";
+// this is a comment
+/* this
+is 
+a multiline
+comment
+*/
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <button>order pizza</button>
+</body>
+</html>
